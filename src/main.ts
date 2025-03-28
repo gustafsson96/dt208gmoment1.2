0c18f17a1211edc0b3f305ef930724de870979b0
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-03-28 22:08:22 +0100

    Create interface for courses

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,11 @@ import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 app!.innerHTML = '<h1>Hello, TypeScript!</h1>';
+
+// Interface for courses
+interface Course {
+  code: number;
+  name: string;
+  progression: "A" | "B" | "C";
+  syllabus: string;
+}
