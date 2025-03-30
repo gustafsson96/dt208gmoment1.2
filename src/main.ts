ea8a28ff608b555c1d6be4ea2b928a7d92a85581
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-03-30 22:02:16 +0200

    Create form and function to add a new course

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './style.css';
 
-document.addEventListener("DOMContentLoaded", fetchCourses);
+document.addEventListener("DOMContentLoaded", () => {
+  fetchCourses();
+  const form = document.getElementById("add-course-form") as HTMLFormElement;
+  form.addEventListener("submit", addCourse);
+});
 
 // Interface for courses
 interface CourseInfo {
-  code: number;
+  code: string;
   coursename: string;
   progression: "A" | "B" | "C";
   syllabus: string;
@@ -27,7 +31,61 @@ async function fetchCourses(): Promise<void> {
   showCourses(courses);
 }
 
-/* Function to delete a course from the list */
+/* Function to add a new course to the list */
+function addCourse(event: Event): void {
+  event.preventDefault();
+
+  // Get form input values
+  const courseCodeInput = document.getElementById("course-code") as HTMLInputElement;
+  const courseCode = courseCodeInput.value;
+  const courseNameInput = document.getElementById("course-name") as HTMLInputElement;
+  const courseName = courseNameInput.value;
+  const courseProgressionInput = document.getElementById("course-progression") as HTMLInputElement;
+  const courseProgression = courseProgressionInput.value as "A" | "B" | "C";
+  const syllabusInput = document.getElementById("course-syllabus") as HTMLInputElement;
+  const courseSyllabus = syllabusInput.value;
+
+  // Get courses from local storage
+  let courses: CourseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
+
+  // Check if course code exists
+  const isCodeUnique = !courses.some(course => course.code === courseCode);
+  if (!isCodeUnique) {
+    alert("Kurskoden måste vara unik.");
+    return;
+  }
+
+  if (courseCode.length !== 6) {
+    alert("Kurskoden måste innehålla 6 tecken.");
+    return;
+  }
+
+  if (!["A", "B", "C"].includes(courseProgression)) {
+    alert("Kursprogression måste vara A, B eller C.");
+    return;
+  }
+
+  // new course object
+  const newCourse: CourseInfo = {
+    code: courseCode,
+    coursename: courseName,
+    progression: courseProgression, 
+    syllabus: courseSyllabus,
+  };
+
+
+  // add new course
+  courses.push(newCourse);
+
+  // update list of courses to local storage
+  localStorage.setItem("courses", JSON.stringify(courses));
+
+  showCourses(courses);
+}
+
+document.getElementById("course-form")
+
+/* Function to remove a course from the list */
 function removeCourse(index: number): void {
   let courses: CourseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
 
@@ -63,12 +121,5 @@ courseBody.addEventListener("click", (event) => {
     removeCourse(index);
   }
 });
-}
-
-
-/* Function to add a new course to the list */
-
-function addCourse() {
-
 }
 
