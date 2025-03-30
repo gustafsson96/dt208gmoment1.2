5b800ab190dbba0c50be3402b528df37f351858e
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-03-30 18:01:10 +0200

    Create function to fetch courses from API when localstorage is empty

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,28 @@ interface CourseInfo {
 }
 
 /**
- * Function to fetch course information initially
+ * Function to fetch courses from API only when local storage is empty
  */
 async function fetchCourses(): Promise<void> {
-  
+  let courses: CourseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
+
+  if (courses.length === 0) {
+    try {
+      const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+      const courses: CourseInfo[] = await response.json();
+
+      localStorage.setItem("courses", JSON.stringify(courses));
+    } catch (error) {
+      console.error("Course fetch failed:", error)
+    }
+  }
+  showCourses(courses);
 }
 
 /**
  * Function that displays courses to the screen
 */
-function showCourses() {
+function showCourses(courses: CourseInfo[]): void {
   const courseList = document.getElementById("course-list") as HTMLUListElement;
 }
 
